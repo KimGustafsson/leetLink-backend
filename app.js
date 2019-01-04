496431708c8c2519972170022c89cdb53cf5ff4e
Kim Gustafsson <Kim.Gustafsson@cybercom.com>
2019-01-04 10:21:44 +0100

    Add cryptojs and base62 packages

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = '9000';
 const morgan = require('morgan');
+const cryptojs = require('crypto-js');
+const base62 = require('base62/lib/ascii');
 
 app.use(morgan('short'));
 
@@ -11,8 +13,9 @@ app.get('/', (req, res) => {
 
 // Generate shorter url and put pair in DB
 app.get('/generate/:url', (req, res) => {
+   let encryptedUrl = cryptojs.MD5(req.params.url);
    console.log('Generate new url for: ', req.params.url);
-   res.send(req.params.url);
+   res.send(encryptedUrl);
 });
 
 // Fetch url from DB
