73972734e38505bd8f97645d39fe18b04dbc09fb
Kim Gustafsson <Kim.Gustafsson@cybercom.com>
2019-01-06 11:38:54 +0100

    Use randomstring and mongodb

--- app.js
+++ app.js
@@ -2,24 +2,35 @@ const express = require('express');
 const app = express();
 const port = '9000';
 const morgan = require('morgan');
-const cryptojs = require('crypto-js');
-const base62 = require('base62/lib/ascii');
+const randomstring = require('randomstring');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const dbUrl = 'mongodb://localhost:27017';
+const dbName = 'leetLinkDB';
 
+MongoClient.connect(dbUrl, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
 app.use(morgan('short'));
 
-app.get('/', (req, res) => {
-   res.send('Hello from the root!');
-});
-
 // Generate shorter url and put pair in DB
-app.get('/generate/:url', (req, res) => {
-   let encryptedUrl = cryptojs.MD5(req.params.url);
+app.get('/api/generate/:url', (req, res) => {
    console.log('Generate new url for: ', req.params.url);
-   res.send(encryptedUrl);
+   res.send(getRandomId());
+   // put both urls in the DB
+   let temp = new urlObj(req.params.url, getRandomId());
+   console.log(temp);
+
+   // Send the generated url
 });
 
 // Fetch url from DB
-app.get('/fetch/:shortUrl', (req, res) => {
+app.get('/api/fetch/:shortUrl', (req, res) => {
    console.log('Fetching url from: ', req.params.shortUrl);
    res.send(req.params.shortUrl);
 });
@@ -27,3 +38,29 @@ app.get('/fetch/:shortUrl', (req, res) => {
 app.listen(port, () => {
    console.log('leetLink server is running on port', port);
 });
+
+const getRandomId = () => {
+   return randomstring.generate({
+      length: 6,
+      charset: 'alphanumeric',
+   });
+}
+
+const saveToDB = (db, urlObj, callback) => {
+   const collection = db.collection('urls');
+   collection.insertOne({
+      originalUrl: urlObj.originalUrl,
+      generatedString: urlObj.generatedString
+   }, (err, result) => {
+      assert.equal(err, null);
+      console.log('Inserted url to db');
+      callback(result);
+   });
+}
+
+class urlObj {
+   constructor(originalUrl, generatedString) {
+      this.originalUrl = originalUrl;
+      this.generatedString = generatedString;
+   }
+}
